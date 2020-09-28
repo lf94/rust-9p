f082dd52dd472f5dfce4af984660174fec7b53b4
Travis CI User <travis@example.org>
2020-09-28 18:16:41 +0000

    Update documentation

---
+++ implementors/core/ops/try/trait.Try.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["rs9p"] = [{"text":"impl&lt;U&gt; Try for SResult&lt;U&gt;","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
